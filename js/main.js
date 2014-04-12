d91e76deb59ea4e61d1497cbe73652a9df491978
AB5 <newbox.me@gmail.com>
2014-04-11 20:15:20 -0400

    another mjr restyle

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+$("#chat-close").click(function(e) {
+    e.preventDefault();
+    $("#chat-wrapper").toggleClass("active");
+});
+
+
+$("#chat-toggle").click(function(e) {
+    e.preventDefault();
+    $("#chat-wrapper").toggleClass("active");
+});
+
+
+$("#mainContainer" ).load("views/frontpage.html");
+
+$("#home").click(function () {
+   $("#mainContainer" ).load("views/frontpage.html");
+    });
+$("#nav2").click(function () {
+    $("#mainContainer").html("").load("./views/contacts.html")
+    });
+$("#nav3").click(function () {
+    $("#mainContainer").html("").load("./views/products.html")
+    });
+$("#nav4").click(function () {
+    $("#mainContainer").html("").load("./views/products.html")
+    });
